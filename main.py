f833c5185256981aa18fe9c3e0ca6b6be25a2f72
M0vi <movizinha@gmail.com>
2025-02-02 12:52:35 -0300

    main.py

--- main.py
+++ main.py
@@ -1,55 +1,86 @@
-import requests
+
+import aiohttp
+import asyncio
+from itertools import cycle
 from colorama import Fore, Style
 import os
+import random
+import unicodedata
 
 
-def validate_username(username):
+def load_proxies(filename):
+    with open(filename, 'r') as file:
+        proxies = file.read().splitlines()
+    return cycle(proxies)
+
+
+def remove_special_characters(username):
+    # Remover acentos e caracteres especiais
+    nfkd = unicodedata.normalize('NFKD', username)
+    return ''.join([c for c in nfkd if unicodedata.category(c) != 'Mn'])
+
+
+async def validate_username(session, username, proxy):
+    username = remove_special_characters(username)  # Limpar nome de caracteres especiais
     url = f"https://auth.roblox.com/v1/usernames/validate?birthday=2006-09-21T07:00:00.000Z&context=Signup&username={username}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        if data['code'] == 0:
-            print(f"{Fore.GREEN}The username '{username}' is valid and available{Style.RESET_ALL}")
-            with open('valid.txt', 'a') as file:
-                file.write(username + '\n')
-        elif data['code'] == 1:
-            print(f"{Fore.RED}The username '{username}' is already in use{Style.RESET_ALL}")
-        elif data['code'] == 2:
-            print(f"{Fore.RED}The username '{username}' is not appropriate for Roblox{Style.RESET_ALL}")
-        elif data['code'] == 10:
-            print(f"{Fore.YELLOW}The username '{username}' might contain private information{Style.RESET_ALL}")
-    else:
-        print(f"{Fore.RED}Unable to access Roblox API{Style.RESET_ALL}")
+    proxy_url = f"http://{proxy}"
+    try:
+        async with session.get(url, proxy=proxy_url) as response:
+            if response.status == 200:
+                data = await response.json()
+                if data['code'] == 0:
+                    print(f"{Fore.GREEN}O usuário '{username}' está disponível!{Style.RESET_ALL}")
+                    with open('valid.txt', 'a') as file:
+                        file.write(username + '\n')
+                    print(f"{Fore.CYAN}Salvo '{username}' para 'valid.txt'{Style.RESET_ALL}")
+                elif data['code'] == 1:
+                    print(f"{Fore.RED}O usuário '{username}' já está em uso{Style.RESET_ALL}")
+                elif data['code'] == 2:
+                    print(f"{Fore.RED}O usuário '{username}' é inapropriado{Style.RESET_ALL}")
+                elif data['code'] == 10:
+                    print(f"{Fore.YELLOW}O usuário '{username}' provavelmente contém informações privadas{Style.RESET_ALL}")
+            else:
+                print(f"{Fore.RED}Falha ao checar usuário '{username}' (Status: {response.status}){Style.RESET_ALL}")
+    except Exception as e:
+        print(f"{Fore.RED}Falha ao checar usuário '{username}' com o proxy {proxy}: {e}{Style.RESET_ALL}")
 
 
-def validate_usernames_from_file(filename):
-    with open(filename, "r") as file:
-        usernames = file.read().splitlines()
-    for username in usernames:
-        validate_username(username)
+async def validate_usernames_from_file(filename, proxies):
+    async with aiohttp.ClientSession() as session:
+        with open(filename, "r", encoding="utf-8") as file:  # Especificando a codificação utf-8
+            usernames = file.read().splitlines()
+
+        random.shuffle(usernames)
+
+        tasks = []
+        for username in usernames:
+            username = remove_special_characters(username)
+            proxy = next(proxies)
+            tasks.append(validate_username(session, username, proxy))
+        await asyncio.gather(*tasks)
 
 
-while True:
-    print()
-    os.system('cls' if os.name == 'nt' else 'clear') # clear console
-    print(f"{Fore.BLUE}██   ██  ██████ ██   ██ ███████  ██████ ██   ██ ███████ ██████  ")
-    print(f" ██ ██  ██      ██   ██ ██      ██      ██  ██  ██      ██   ██ ")
-    print(f"  ███   ██      ███████ █████   ██      █████   █████   ██████  ")
-    print(f" ██ ██  ██      ██   ██ ██      ██      ██  ██  ██      ██   ██ ")
-    print(f"██   ██  ██████ ██   ██ ███████  ██████ ██   ██ ███████ ██   ██ {Style.RESET_ALL}")
-    print()
-    print(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Choose an option:")
-    print(f"{Fore.MAGENTA}[{Fore.RESET}1{Fore.MAGENTA}]{Fore.RESET} Manually enter a username")
-    print(f"{Fore.MAGENTA}[{Fore.RESET}2{Fore.MAGENTA}]{Fore.RESET} Check a list of usernames from a file")
-    print(f"{Fore.MAGENTA}[{Fore.RESET}0{Fore.MAGENTA}]{Fore.RESET} Exit")
-    choice = input(f"{Fore.MAGENTA}[{Fore.RESET}>{Fore.MAGENTA}]{Fore.RESET} ")
-    if choice == '1':
-        username = input(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Enter a username: ")
-        validate_username(username)
-    elif choice == '2':
-        filename = input(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Enter the filename of the usernames file (must include .txt): ")
-        validate_usernames_from_file(filename)
-    elif choice == '0':
-        break
-    else:
-        print(f"{Fore.RED}Invalid choice{Style.RESET_ALL}")
+async def main():
+    proxies = load_proxies("proxies.txt")
+    while True:
+        print(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Escolha uma opção:")
+        print(f"{Fore.MAGENTA}[{Fore.RESET}1{Fore.MAGENTA}]{Fore.RESET} Colocar nome manualmente")
+        print(f"{Fore.MAGENTA}[{Fore.RESET}2{Fore.MAGENTA}]{Fore.RESET} Checar lista de nomes")
+        print(f"{Fore.MAGENTA}[{Fore.RESET}0{Fore.MAGENTA}]{Fore.RESET} Sair")
+        choice = input(f"{Fore.MAGENTA}[{Fore.RESET}>{Fore.MAGENTA}]{Fore.RESET} ")
+        if choice == '1':
+            username = input(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Digite um nome: ")
+            async with aiohttp.ClientSession() as session:
+                proxy = next(proxies)
+                await validate_username(session, username, proxy)
+        elif choice == '2':
+            filename = input(f"{Fore.MAGENTA}[{Fore.RESET}+{Fore.MAGENTA}]{Fore.RESET} Digite o nome da pasta (deve incluir o formato da extensão; tipo .txt): ")
+            await validate_usernames_from_file(filename, proxies)
+        elif choice == '0':
+            break
+        else:
+            print(f"{Fore.RED}Inválido{Style.RESET_ALL}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
